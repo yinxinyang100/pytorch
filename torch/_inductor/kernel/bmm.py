46e144fd2bb10ce2704d9da99888a55083f28036
Jack Taylor <jack.taylor@amd.com>
2025-02-04 13:21:53 +0000

    bmm fix

--- torch/_inductor/kernel/bmm.py
+++ torch/_inductor/kernel/bmm.py
@@ -37,6 +37,9 @@ def bmm_grid(b, m, n, meta):
 
 
 def _is_large_block_for_cpu(m, n, k):
+    # Thresholds are experimentally determined to reduce Triton CPU compile times
+    if m > 128 or n > 128 or k > 128:
+        return True
     return m * n > 2**12
 
 
